324e5ed1b5324d744eb89667a622a1b883ad757f
Kiselevich Stanislav <kiselevich.stasik@gmail.com>
2022-02-18 22:13:23 +0300

    feat: add full project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let result;
+    if(n < 0){
+        n = n * (-1)
+    }
+    result = n.toString().split('').reverse().join('')
+    return Number(result)
 }
